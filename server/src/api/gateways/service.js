d9161ce03c211d24172dbe8dc7bbb05ffa18e95b
Nielsen Alonso <nielsenwebdev@gmail.com>
2022-10-25 18:13:16 -0400

    create gateway test fixed

--- server/src/api/gateways/service.js
+++ server/src/api/gateways/service.js
@@ -3,14 +3,12 @@ import { validateIP } from '../../utils/validateIP';
 
 const service = {
 	async create(data) {
+		if (!data.name || !data.ip) throw new Error('Invalid input data.');
 		if (JSON.stringify(data) === '{}') throw new Error('Invalid input data.');
-		if (!(data instanceof Gateway)) throw new Error('Invalid input data.');
-		if (data && !validateIP(data)) throw new Error('Invalid ip address.');
+		if (!validateIP(data.ip)) throw new Error('Invalid ip address.');
 
 		const newGateway = new Gateway(data);
-		const result = await newGateway.save();
-		console.log(result);
-		return result;
+		return newGateway.save();
 	},
 };
 
--- server/src/api/gateways/service.test.js
+++ server/src/api/gateways/service.test.js
@@ -1,5 +1,9 @@
+import mongoose from 'mongoose';
+
 import { Gateway } from './model';
 import { service } from './service';
+
+mongoose.connect('mongodb://localhost/gateways');
 
 describe('create', () => {
 	test('create gateway properly', async () => {
@@ -7,12 +11,18 @@ describe('create', () => {
 			name: 'calimete',
 			ip: '192.168.1.1',
 		});
-		console.log(result);
+
 		expect(result instanceof Gateway).toBeTruthy();
 	});
 
 	test('entry an empty object', async () => {
 		await expect(service.create({})).rejects.toThrow('Invalid input data.');
+	});
+
+	test('entry invalid ip address', async () => {
+		await expect(
+			service.create({ name: 'ss', ip: '192.300.400.500' })
+		).rejects.toThrow('Invalid ip address.');
 	});
 
 	test('entry anything else but an object', async () => {
